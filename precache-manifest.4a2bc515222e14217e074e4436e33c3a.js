c5a87ff4de8046a967b0815c0e1bf4a5cc688aa5
CHO JUNSEOK <CHOJUNSEOK@USERnoMacBook-Pro.local>
2019-10-22 15:41:20 +0900

    Updates

---
+++ precache-manifest.4a2bc515222e14217e074e4436e33c3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1466de7e34ae02c2a924fdc11e1fd820",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "639ec5ab9f1864a2e710",
+    "url": "/movie_app/static/css/main.3c32bad2.chunk.css"
+  },
+  {
+    "revision": "06e3e7e1964695cad340",
+    "url": "/movie_app/static/js/2.77edc3c2.chunk.js"
+  },
+  {
+    "revision": "639ec5ab9f1864a2e710",
+    "url": "/movie_app/static/js/main.59f92b7a.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
